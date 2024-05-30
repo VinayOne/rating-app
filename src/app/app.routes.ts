95baa3ec66507461ff2ba4a97837158b94f53d39
vinayone <vinaymunda@outlook.com>
2024-05-30 19:36:31 +0530

    components, angular material & bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { QuestionComponent } from './components/question/question.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'rating', component: QuestionComponent
+    },
+    {
+        path: '', redirectTo: (urlInfo) => {
+            console.info(urlInfo);
+            return '/rating';
+        },
+        pathMatch: 'full'
+    }
+];
